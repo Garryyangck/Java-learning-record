65aa835a765bfefba73da026c419e30678d17818
Garry <yanggary0127@outlook.com>
2024-05-27 22:44:48 +0800

    第117天打卡

--- LeetCode/DataStructure/DataStructure.md
+++ LeetCode/DataStructure/DataStructure.md
@@ -7,3 +7,96 @@
 3. 红黑树和2-3树是等价的，因此由于2-3树是绝对平衡的，因此红黑树根节点到任意一个空节点所经过的黑色节点（一个黑色节点代表要么是一个2节点，要么是一个3节点）总数是相同的。
 
 4. 红黑树是一颗“黑平衡的树”，在添加和删除节点的过程中比AVL树有更好的性能。
+
+---
+
+
+
+## 并查集
+
+1. 对于一组数据，支持两种操作：
+
+	```java
+	union(E p, E q);
+	isConnected(E p, E q);
+	// 之所以使用 E，是因为并查集中并不需要考虑两个元素的类型是什么，只需体现它们之间的连接关系
+	```
+
+2. ==基于 Quick Find 的实现思路==：
+
+	> find O(1)，union O(n)
+	>
+	> 1. <img src="DataStructure.assets/image-20240527210420638.png" alt="image-20240527210420638" style="zoom:50%;" />
+	>
+	> 2. find 函数，查询 index 数据所属的集合
+	>
+	> 	```java
+	> 	find(E q);
+	> 	```
+	>
+	> 3. ```java
+	> 	public UnionFind(int n) {
+	> 	    parent = new int[n];
+	> 	    for (int i = 0; i < n; i++) {
+	> 	        parent[i] = i; // 每个元素最开始没有连接的时候，都独属于一个集合
+	> 	    }
+	> 	}
+	> 	```
+	>
+	> 4. 假如上图中的 0 和 5 union，那么它们所属集合的所有元素也会相连，表现是最终所有元素相连。
+
+3. 如果满足 `find(p) == find(q)`，那么就可以认为 p 和 q 所属同一个集合，即 `isConnected`。
+
+4. ==基于 Quick Union 的实现思路（常用思路）==：
+
+	> 1. <img src="DataStructure.assets/image-20240527213152360.png" alt="image-20240527213152360" style="zoom:33%;" />
+	>
+	> 2. ==执行 union 后的结果==：
+	>
+	> 	<img src="DataStructure.assets/image-20240527213252319.png" alt="image-20240527213252319" style="zoom:33%;" />
+	>
+	> 3. <img src="DataStructure.assets/image-20240527213330144.png" alt="image-20240527213330144" style="zoom:50%;" />
+	>
+	> 4. 最终表现为树的结果，其中根节点的 parent 为自己，即指向自己。
+	>
+	> 	<img src="DataStructure.assets/image-20240527213651921.png" alt="image-20240527213651921" style="zoom:50%;" />
+	>
+	> 5. Union O(h)，Find O(h)，其中 h 为树的高度。
+	>
+	> 6. ```java
+	> 	class UnionFind {
+	> 	    int[] parent;
+	> 	
+	> 	    public UnionFind(int n) {
+	> 	        parent = new int[n];
+	> 	        for (int i = 0; i < n; i++)
+	> 	            parent[i] = i; // 每个元素最开始没有连接的时候，都指向自己，表示独立
+	> 	    }
+	> 	
+	> 	    /*
+	> 	     * 联合，本质就是将它们各自唯一的根节点连接起来
+	> 	     */
+	> 	    public void union(int index1, int index2) {
+	> 	        parent[find(index2)] = find(index1); // index2 的根节点的父节点设置为 index1 的根节点
+	> 	    }
+	> 	
+	> 	    public boolean isConnected(int index1, int index2) {
+	> 	        return find(index1) == find(index2);
+	> 	    }
+	> 	
+	> 	    /*
+	> 	     * 查询 index 的根节点
+	> 	     */
+	> 	    p int find(int index) {
+	> 	        if (parent[index] != index) // 父节点不是自己，即自己不是根节点，查询父节点的根节点
+	> 	            return parent[index] = find(parent[index]);
+	> 	        else
+	> 	            return index;
+	> 	    }
+	> 	}
+	> 	```
+
+---
+
+
+
--- LeetCode/leetcode-helper/src/main/java/garry/leetcode/Main.java
+++ LeetCode/leetcode-helper/src/main/java/garry/leetcode/Main.java
@@ -1,9 +1,6 @@
 package garry.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Garry
@@ -12,80 +9,115 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<List<String>> accounts = new ArrayList<>();
+        ArrayList<String> account1 = new ArrayList<>();
+        account1.add("John");
+        account1.add("johnsmith@mail.com");
+        account1.add("john00@mail.com");
+        ArrayList<String> account2 = new ArrayList<>();
+        account2.add("John");
+        account2.add("johnnybravo@mail.com");
+        ArrayList<String> account3 = new ArrayList<>();
+        account3.add("John");
+        account3.add("johnsmith@mail.com");
+        account3.add("john_newyork@mail.com");
+        ArrayList<String> account4 = new ArrayList<>();
+        account4.add("Mary");
+        account4.add("mary@mail.com");
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+        accounts.add(account4);
+        System.out.println(new Solution().accountsMerge(accounts));
     }
 }
 
-class LFUCache {
-    class DLinkedNode {
-        int key;
-        int value;
-        int cnt = 0;
-        DLinkedNode pre;
-        DLinkedNode next;
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        int n = accounts.size();
+        // 创建并查集
+        UnionFind uf = new UnionFind(n);
+        // 便于我们做并查集的合并
+        Map<String/* email */, Integer/* index of username */> map = new HashMap<>();
 
-        public DLinkedNode() {
-        }
-
-        public DLinkedNode(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-    private final Map<Integer/* key */, DLinkedNode/* specific node */> map = new HashMap<>();
-    private final Map<Integer/* count */, List<DLinkedNode>/* count list */> counter = new HashMap<>();
-    private int minCnt = Integer.MAX_VALUE;
-    private int size;
-    private final int capacity;
-
-    public LFUCache(int capacity) {
-        this.size = 0;
-        this.capacity = capacity;
-    }
-
-    public int get(int key) {
-        if (map.containsKey(key)) {
-            addCnt(map.get(key));
-            return map.get(key).value;
-        } else {
-            return -1;
-        }
-    }
-
-    public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            DLinkedNode node = map.get(key);
-            node.value = value;
-            addCnt(node);
-        } else {
-            if (size >= capacity) {
-                DLinkedNode delNode = counter.get(minCnt).get(0);
-                map.remove(delNode.key);
-                counter.get(minCnt).remove(0);
-                if (counter.get(minCnt).isEmpty())
-                    counter.remove(minCnt);
-                size--;
+        // 将并查集中的元素进行合并
+        for (int index = 0; index < n; index++) {
+            List<String> account = accounts.get(index);
+            account = account.subList(1, account.size());
+            for (String email : account) {
+                if (!map.containsKey(email)) { // email 没有重复
+                    map.put(email, index);
+                } else { // email 出现重复，即需要合并
+                    uf.union(index, map.get(email));
+                }
             }
-            DLinkedNode newNode = new DLinkedNode(key, value);
-            map.put(key, newNode);
-            addCnt(newNode);
-            size++;
         }
+
+        // 合并相同账户
+        List<List<String>> ans = new ArrayList<>();
+        Map<Integer/* 根节点 */, Integer/* 在ans中的下标位置 */> mp = new HashMap<>();
+        for (int index = 0; index < n; index++) {
+            int rootIndex = uf.find(index);
+            if (!mp.containsKey(rootIndex)) {
+                List<String> account = new ArrayList<>(accounts.get(index));
+                mp.put(rootIndex, ans.size());
+                ans.add(account);
+            } else { // 已有相同根节点插入，当前账户和那个账户合并
+                List<String> oldAccount = ans.get(mp.get(rootIndex));
+                List<String> addAccount = new ArrayList<>(accounts.get(index));
+                for (int i = 1; i < addAccount.size(); i++)
+                    oldAccount.add(addAccount.get(i));
+            }
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        // 排序并去重 email
+        for (List<String> account : ans) {
+            List<String> emails = account.subList(1, account.size());
+            emails.sort(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareTo(o2);
+                }
+            });
+            List<String> newAccount = new ArrayList<>();
+            newAccount.add(account.get(0));
+            for (int i = 0; i < emails.size(); i++) {
+                if (i > 0 && emails.get(i).equals(emails.get(i - 1))) {
+                    continue;
+                } else {
+                    newAccount.add(emails.get(i));
+                }
+            }
+            res.add(newAccount);
+        }
+        return res;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i; // 每个元素最开始没有连接的时候，都指向自己，表示独立
     }
 
-    private void addCnt(DLinkedNode node) {
-        List<DLinkedNode> preNodeList = counter.get(node.cnt);
-        if (preNodeList != null)
-            preNodeList.remove(node);
-        if (minCnt == node.cnt && (preNodeList == null || preNodeList.size() == 0))
-            minCnt++;
+    /*
+     * 联合，本质就是将它们各自唯一的根节点连接起来
+     */
+    public void union(int index1, int index2) {
+        parent[find(index2)] = find(index1); // index2 的根节点的父节点设置为 index1 的根节点
+    }
+
+    /*
+     * 查询 index 的根节点
+     */
+    public int find(int index) {
+        if (parent[index] != index) // 父节点不是自己，即自己不是根节点，查询父节点的根节点
+            return parent[index] = find(parent[index]);
         else
-            minCnt = Math.min(minCnt, node.cnt + 1);
-        node.cnt++;
-        List<DLinkedNode> nodeList = counter.get(node.cnt);
-        if (nodeList == null)
-            nodeList = new ArrayList<>();
-        nodeList.add(node);
-        counter.put(node.cnt, nodeList);
+            return index;
     }
 }
