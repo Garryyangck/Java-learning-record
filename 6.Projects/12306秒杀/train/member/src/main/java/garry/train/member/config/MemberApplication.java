3c39b9616eac0733a2c7177ee8d607ba52d4eb31
Garry <yanggary0127@outlook.com>
2024-09-02 12:22:19 +0800

    2024-9-2 12:16 1. 初始化项目: 配置文件和控制台编码类型为utf-8; 热部署(自动构建项目); 自动导包和删包。 2. 新增会员模块: 将train/pom.xml中的dependencies和build移动到member/pom.xml; 并把train下的源码移动到member中，多创建了一个config包用于存放MemberApplication，因此需要在MemberApplication中新增一个@ComponentScan注解，用于额外扫描garry包下的所有文件(否则只会扫描MemberApplication本包，即config包下的文件，扫描不到controller)。

---
+++ 6.Projects/12306秒杀/train/member/src/main/java/garry/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package garry.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("garry") // 由于Application类放到了config包下，它只能扫描和自己同包的类，因此需要新增ComponentScan注解让其扫描整个garry包下的类
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
