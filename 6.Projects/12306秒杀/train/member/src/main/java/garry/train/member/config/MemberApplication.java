3d4a551d5f8e0c0fafa63fb9ce981ed33bea8551
Garry <yanggary0127@outlook.com>
2024-09-03 21:21:21 +0800

    2024-9-3 21:19 1. Application类中创建启动日志。 2. 创建logback-spring.xml，进行控制台日志格式的配置，以及trace和error级别日志的持久化配置。 3. 在.gitignore中忽略"log/"文件夹

--- 6.Projects/12306秒杀/train/member/src/main/java/garry/train/member/config/MemberApplication.java
+++ 6.Projects/12306秒杀/train/member/src/main/java/garry/train/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package garry.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan("garry") // 由于Application类放到了config包下，它只能扫描和自己同包的类，因此需要新增ComponentScan注解让其扫描整个garry包下的类
 public class MemberApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+    public static void main(String[] args)   {
+        // 打印启动日志
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment(); // 注意，这一句已经app.run了，因此不能再额外run了
+        log.info("启动成功！");
+        log.info("地址\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        // 包括项目的一些说明文档，测试文档，都可以打印在这里
     }
-
 }
