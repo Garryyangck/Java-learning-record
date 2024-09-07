a6dcda935e4fdefe9e0114a4251223ead5dfb2c4
Garry <yanggary0127@outlook.com>
2024-09-07 18:17:01 +0800

    2024-9-7 18:16 1. 集成 ant design vue，引入所有Icons图标库。

--- 6.Projects/12306秒杀/train/web/src/main.js
+++ 6.Projects/12306秒杀/train/web/src/main.js
@@ -2,5 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue' // 全局引入 ant-design-vue 的所有组件
+import 'ant-design-vue/dist/antd.css' // 全局引入 ant-design-vue 的 css
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(Antd);
+
+// // 全局使用所有的图标
+// const icons = Icons;
+// for (const i in icons) { // 遍历引入所有的图标
+//     app.component(i, icons[i]);
+// }
+
+// 全局注册图标组件
+Object.keys(Icons).forEach((key) => {
+    app.component(key, Icons[key]);
+});
+
+app.mount('#app');
