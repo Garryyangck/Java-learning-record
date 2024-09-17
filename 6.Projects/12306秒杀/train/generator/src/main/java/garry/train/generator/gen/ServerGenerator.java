700e36f378a3ec2f41fd4fd9142435a74857b9a5
Garry <yanggary0127@outlook.com>
2024-09-17 19:20:15 +0800

    2024-9-17 18:57 1. 集成 dom4j，使用 XPATH 读取 generator/pom.xml 中的指定标签的内容，比如 "//pom:configurationFile"。 2. 通过读取 generatorPath 得到 tableName 和 domainObjectName。

--- 6.Projects/12306秒杀/train/generator/src/main/java/garry/train/generator/gen/ServerGenerator.java
+++ 6.Projects/12306秒杀/train/generator/src/main/java/garry/train/generator/gen/ServerGenerator.java
@@ -1,16 +1,63 @@
 package garry.train.generator.gen;
 
-import garry.train.generator.util.FreemarkerUtil;
+import lombok.Data;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.util.HashMap;
 
 public class ServerGenerator {
-    private static String toPath = "generator/src/main/java/garry/train/generator/test/";
+    @Data
+    private static class GeneratorResult {
+        private String tableName;
+        private String  domainObjectName;
+
+        public GeneratorResult(String tableName, String domainObjectName) {
+            this.tableName = tableName;
+            this.domainObjectName = domainObjectName;
+        }
+    }
+
+    private static String pomPath = "generator/pom.xml/";
 
     public static void main(String[] args) throws Exception {
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", map);
+        String generatorPath = getGeneratorPath();
+        GeneratorResult generatorResult = getGeneratorResult(generatorPath);
+
+    }
+
+    private static GeneratorResult getGeneratorResult(String generatorPath) throws DocumentException {
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println("table = " + table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+        return new GeneratorResult(tableName.getText(), domainObjectName.getText());
+    }
+
+    /**
+     * 从 generator/pom.xml/ 中获取 Mybatis-generator 配置文件的路径
+     */
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        /*
+          使用 XPATH 在 xml 文件中找寻所需的标签
+          解释 "//pom:configurationFile"：
+          // : 从根目录下寻找
+          pom : xml 的命名空间
+          configurationFile : 节点名
+          若要找 configurationFile 下的某属性，就用:
+          Node.selectSingleNode("@propertyName")
+         */
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println("generatorPath = " + node.getText());
+        return node.getText();
     }
 }
