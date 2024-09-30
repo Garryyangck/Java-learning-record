bae257a1dd08e3fb97f917c9413960543289e5d9
Garry <yanggary0127@outlook.com>
2024-09-30 14:47:34 +0800

    1. 增加每日余票的redisKey，希望到时候有mysql和redis两个实现类。

--- 6.Projects/12306秒杀/train/common/src/main/java/garry/train/common/consts/RedisConst.java
+++ 6.Projects/12306秒杀/train/common/src/main/java/garry/train/common/consts/RedisConst.java
@@ -10,12 +10,19 @@ package garry.train.common.consts;
  */
 public class RedisConst {
     /**
-     * redis存储验证码的 key，其中 %s为 mobile(之后可以改为 member.Id，因为 mobile容易暴露用户的手机号)
+     * redis 存储验证码的 key，其中 %s为 mobile (之后可以改为 member.Id，因为 mobile 容易暴露用户的手机号)
+     * 举例说明: code:phone_number:17380672612
      */
-    public static final String CODE_FORMAT = "code:phoneNumber:%s";
+    public static final String CODE_FORMAT = "code:phone_number:%s";
 
     /**
-     * 验证码 key的过期时间
+     * 验证码 key 的过期时间
      */
     public static final Integer CODE_EXPIRE_SECOND = 60 * 5;
+
+    /**
+     * redis 存储每日余票信息的 key
+     * 举例说明: daily_ticket:2024-10-14:G000224:北京~南京
+     */
+    public static final String DAILY_TRAIN_TICKET_FORMAT = "daily_ticket:%s:%s:%s~%s";
 }
--- 6.Projects/12306秒杀/train/common/src/main/java/garry/train/common/util/RedisUtil.java
+++ 6.Projects/12306秒杀/train/common/src/main/java/garry/train/common/util/RedisUtil.java
@@ -1,6 +1,9 @@
 package garry.train.common.util;
 
+import cn.hutool.core.date.DateUtil;
 import garry.train.common.consts.RedisConst;
+
+import java.util.Date;
 
 /**
  * @author Garry
@@ -12,5 +15,12 @@ public class RedisUtil {
      */
     public static String getRedisKey4Code(String mobile) {
         return String.format(RedisConst.CODE_FORMAT, mobile);
+    }
+
+    /**
+     * 获取每日余票的 redisKey
+     */
+    public static String getRedisKey4DailyTicket(Date date, String trainCode, String start, String end) {
+        return String.format(RedisConst.DAILY_TRAIN_TICKET_FORMAT, DateUtil.format(date, "yyyy-MM-dd"), trainCode, start, end);
     }
 }
