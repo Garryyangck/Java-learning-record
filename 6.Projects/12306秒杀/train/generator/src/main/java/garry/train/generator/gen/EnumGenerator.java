d56f3c932fe4826b42c976543296e35794f6077a
Garry <yanggary0127@outlook.com>
2024-10-07 20:04:28 +0800

    1. 生成新的枚举类 ConfirmOrderStatusEnum。

--- 6.Projects/12306秒杀/train/generator/src/main/java/garry/train/generator/gen/EnumGenerator.java
+++ 6.Projects/12306秒杀/train/generator/src/main/java/garry/train/generator/gen/EnumGenerator.java
@@ -1,10 +1,7 @@
 package garry.train.generator.gen;
 
 import cn.hutool.core.util.StrUtil;
-import garry.train.generator.enums.SeatColEnum;
-import garry.train.generator.enums.SeatTypeEnum;
-import garry.train.generator.enums.TrainTypeEnum;
-import garry.train.generator.enums.PassengerTypeEnum;
+import garry.train.generator.enums.*;
 
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
@@ -20,7 +17,7 @@ import java.util.List;
  * 2024-09-18 21:22
  */
 public class EnumGenerator {
-    private static String path = "web/src/assets/js/enums.js";
+    private static String path = "admin/src/assets/js/enums.js";
 
     public static void main(String[] args) {
         StringBuffer bufferObject = new StringBuffer();
@@ -31,6 +28,7 @@ public class EnumGenerator {
             toJson(TrainTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatColEnum.class, bufferObject, bufferArray);
+            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
