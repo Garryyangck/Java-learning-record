3c3b37fae9fae4fb146bca10431fabbc8fad8373
Garry <yanggary0127@outlook.com>
2024-10-11 14:24:46 +0800

    1. 生成 message 持久层代码

---
+++ 6.Projects/12306秒杀/train/business/src/main/java/garry/train/business/pojo/Message.java
@@ -0,0 +1,103 @@
+package garry.train.business.pojo;
+
+import java.util.Date;
+
+public class Message {
+    private Long id;
+
+    private Long fromId;
+
+    private Long toId;
+
+    private String type;
+
+    private String status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(Long fromId) {
+        this.fromId = fromId;
+    }
+
+    public Long getToId() {
+        return toId;
+    }
+
+    public void setToId(Long toId) {
+        this.toId = toId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", fromId=").append(fromId);
+        sb.append(", toId=").append(toId);
+        sb.append(", type=").append(type);
+        sb.append(", status=").append(status);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", content=").append(content);
+        sb.append("]");
+        return sb.toString();
+    }
+}
