553f4e5f672cb231cdfa8439b692c34fba88479d
Garry <yanggary0127@outlook.com>
2024-10-21 20:52:53 +0800

    1. 完成 Nacos 配置中心的引入，并通过测试

--- 6.Projects/12306秒杀/train/member/src/main/java/garry/train/member/controller/TestController.java
+++ 6.Projects/12306秒杀/train/member/src/main/java/garry/train/member/controller/TestController.java
@@ -1,14 +1,21 @@
 package garry.train.member.controller;
 
 import garry.train.common.vo.ResponseVo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseVo<String> hello() {
-        return ResponseVo.success("hello member");
+        return ResponseVo.success(testNacos);
     }
 }
