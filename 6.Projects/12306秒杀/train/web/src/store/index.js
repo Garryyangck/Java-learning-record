d44e2af2e4337108d283d217b97a3c69d95153de
Garry <yanggary0127@outlook.com>
2024-10-23 22:18:11 +0800

    把 member 存储到 localStorage 中，让用户关闭浏览器再打开时，也能直接完成登录

--- 6.Projects/12306秒杀/train/web/src/store/index.js
+++ 6.Projects/12306秒杀/train/web/src/store/index.js
@@ -4,7 +4,7 @@ const MEMBER = 'MEMBER';
 
 export default createStore({
   state: { // 定义全局变量
-    member: window.SessionStorage.get(MEMBER) || {},
+    member: window.LocalStorage.get(MEMBER) || {},
     unreadNum: 0
   },
   getters: { // 定义全局变量的get方法
@@ -13,7 +13,7 @@ export default createStore({
   mutations: { // 定义全局变量的set方法
     setMember (state, member) {
       state.member = member;
-      window.SessionStorage.set(MEMBER, member);
+      window.LocalStorage.set(MEMBER, member);
     },
   },
   actions: { // 定义异步方法
